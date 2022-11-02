841cfe9f4d209f66872f4f028733da1e4b09e779
мансур зурабов <asl.zurabov@gmail.com>
2022-11-02 10:45:42 +0300

    slider was added

---
+++ script.js
@@ -0,0 +1,13 @@
+  // import Swiper JS
+import Swiper from 'swiper';
+  // import Swiper styles
+import 'swiper/css';
+    // core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from 'swiper';
+    // import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper();
+  
