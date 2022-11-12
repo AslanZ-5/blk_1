8446c096edd9816771f3bdff4ac9ff2f26e5f01b
мансур зурабов <asl.zurabov@gmail.com>
2022-11-12 21:01:01 +0300

    scroll bar width 768 1020pxs

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
- 
-console.log("dana")
- // import Swiper JS
-import Swiper from 'swiper';
-  // import Swiper styles
-import 'swiper/css';
-    // core version + navigation, pagination modules:
-import Swiper, { Navigation, Pagination } from 'swiper';
-    // import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+const showAllBtn = document.querySelector('.show_all')
+const showImg = document.querySelector('.show_img')
 
 
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+      clickable:true
     },
+
   
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
   });
+
+showAllBtn.addEventListener('click', () => {
+    document.querySelectorAll('.target').forEach((item)=>{
+        item.classList.toggle('hide')
+        
+    });
+    if (!document.querySelectorAll('.target')[0].classList.contains('hide')){
+        showAllBtn.innerHTML = 'Скрыть'
+        showImg.style.transform = 'rotate(180deg)'
+
+    }
+    else{
+        showAllBtn.innerHTML = 'Показать все'
+        showImg.style.transform = 'rotate(0deg)'
+    }
+    
+})
