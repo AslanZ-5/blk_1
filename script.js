a8f1f6ad9897e3e6e1ca60455ab90a16d7baaa63
мансур зурабов <asl.zurabov@gmail.com>
2022-11-15 10:57:58 +0300

    eslint was added

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-const showAllBtn = document.querySelector('.show_all');
-const showImg = document.querySelector('.show_img');
-
+const showAllBtn = document.querySelector('.show_all')
+const showImg = document.querySelector('.show_img')
 
 const swiper = new Swiper('.swiper', {
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable:true
-    },
+// If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
 
-  
-  });
+})
 
 showAllBtn.addEventListener('click', () => {
-    document.querySelectorAll('.target').forEach((item) => {
-        item.classList.toggle('hide');
-        
-    });
-    if (!document.querySelectorAll('.target')[0].classList.contains('hide')){
-        showAllBtn.innerHTML = 'Скрыть';
-        showImg.style.transform = 'rotate(180deg)';
-
-    }
-    else{
-        showAllBtn.innerHTML = 'Показать все';
-        showImg.style.transform = 'rotate(0deg)';
-    }
-    
-});
+  document.querySelectorAll('.target').forEach((item) => {
+    item.classList.toggle('hide')
+  })
+  if (!document.querySelectorAll('.target')[0].classList.contains('hide')) {
+    showAllBtn.innerHTML = 'Скрыть'
+    showImg.style.transform = 'rotate(180deg)'
+  } else {
+    showAllBtn.innerHTML = 'Показать все'
+    showImg.style.transform = 'rotate(0deg)'
+  }
+})
